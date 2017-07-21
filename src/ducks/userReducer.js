979811bbe53b9038c7407df35051d83eca10c0a7
Bobby <ltlbsteed@gmail.com>
2017-07-21 17:20:42 -0600

    update - styling on landing

--- src/ducks/userReducer.js
+++ src/ducks/userReducer.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const getUserURL = 'http://localhost:3005/api/profiles/'
 
 const initialState = {
-  user: []
+  user: [],
+  isLoggedIn: false
 }
 
 //Action Types
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import store from './store';
+// import './normalize.css';
 import './index.scss';
 import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
