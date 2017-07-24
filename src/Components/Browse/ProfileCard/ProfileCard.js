05e93976f56e7efef1d90269e4d002318c6df04e
Bobby <ltlbsteed@gmail.com>
2017-07-24 14:34:02 -0600

    update - userReducer done

--- src/Components/Browse/ProfileCard/ProfileCard.js
+++ src/Components/Browse/ProfileCard/ProfileCard.js
@@ -9,20 +9,23 @@ export default class ProfileCard extends Component {
     return (
       <div>
         <div>
-          <div className="browseImageWrapper"><div className='browseImage' style={{backgroundImage: `url(${this.props.primary_photo})`}}></div></div>
+          <div className="browseImageWrapper">
+            <div className='browseImage' style={{backgroundImage: `url(${this.props.primary_photo})`}}></div>
+          </div>
           <div className="browseNameAge">
-           {this.props.name + "," + " "}
-             {this.props.age}
-           </div>
+            {this.props.name + "," + " "}
+            {this.props.age}
+          </div>
         </div>
-         <div className="profileSlidercontainer">
-            <div className="aboutWrapper"> 
-             {/* <div className="aboutImage"><img src={this.props.primary_photo}/> </div> */}
-             <div className="profileHeader" >
-               <div className="profileFirstName">{this.props.name}</div> 
-            <div className="ageLocation"> {this.props.age} - {this.props.home_town + "," + " "}{this.props.state_code}</div>
+        <div className="profileSlidercontainer">
+          <div className="aboutWrapper">
+            {/* <div className="aboutImage"><img src={this.props.primary_photo}/> </div> */}
+            <div className="profileHeader">
+              <div className="profileFirstName">{this.props.name}</div>
+              <div className="ageLocation"> {this.props.age}
+                - {this.props.home_town + "," + " "}{this.props.state_code}</div>
             </div>
-             
+
             <h4>About Me </h4><p>{this.props.about}</p>
             <hr/>
             <h4>School<span>{this.props.school}</span></h4>
@@ -31,27 +34,25 @@ export default class ProfileCard extends Component {
             <h4>Relationship Readiness<span>{this.props.relationship_readiness}</span></h4>
             <hr/>
             <button id="profileSlideClose">CLose</button>
-              <i id="profileSlideClose" className="fa fa-window-close" aria-hidden="true"></i>
-           </div> 
-           </div>
+            <i id="profileSlideClose" className="fa fa-window-close" aria-hidden="true"></i>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 $(document).ready(function () {
-  
+
   $('#profileSlideClose').click(function (e) {
     e.preventDefault();
     console.log('hello')
     $('.profileSliderContainerOpen').removeClass('profileSlidercontainer')
-  }) 
+  })
 
   $('#profileCheck').click(function (e) {
     e.preventDefault();
     console.log("jquery rules")
     $('.profileSlidercontainer').addClass('profileSliderContainerOpen')
-    
   })
-    
-  }) 
+})
