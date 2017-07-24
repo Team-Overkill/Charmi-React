cb99f708bd5ab09807ecbcc3e23048abebcac72d
Kellen Pugmire <kepugmire@gmail.com>
2017-07-24 10:30:12 -0600

    fixed errors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from './store';
 import './index.scss';
 import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
-import './normalize.css';
+// import './normalize.css';
 
 ReactDOM.render(
 
