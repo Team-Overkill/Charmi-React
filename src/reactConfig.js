a846e0ed5c324b63e2cd92845d35e93af675972a
Jodi Parker <jodilparker@gmail.com>
2017-07-31 11:25:53 -0600

    fixed reactConfig

--- src/reactConfig.js
+++ src/reactConfig.js
@@ -3,4 +3,4 @@ exports.port = 3005;
 exports.backendURL = 'localhost' //or ip of server that is hosting chat
 //Hosted
 // exports.port = 8082;
-// exports.backendURL = 'http://192.241.218.14' //or ip of server that is hosting chat
+// exports.backendURL = '192.241.218.14' //or ip of server that is hosting chat
